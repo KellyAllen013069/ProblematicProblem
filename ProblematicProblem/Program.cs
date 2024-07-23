ba60761dd877102eef32a40c79263bc627b5c687
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2024-07-23 09:56:16 -0500

    fixed error

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -117,7 +117,7 @@ namespace ProblematicProblem
                 Console.WriteLine();
                 int randomNumber = rng.Next(activities.Count);
                 string randomActivity = activities[randomNumber];
-                if (age > 21 && randomActivity == "Wine Tasting")
+                if (age < 21 && randomActivity == "Wine Tasting")
                 {
                     Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
                     Console.WriteLine("Pick something else!");
